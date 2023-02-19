3c2d6e6f39d0215263b740191e9ed852c5593158
Tim Griesser <tgriesser10@gmail.com>
2023-02-18 23:18:19 -0500

    feat: add format options for printing

--- src/codegen/plugin.ts
+++ src/codegen/plugin.ts
@@ -1,20 +1,34 @@
-import type { CodegenPlugin } from '@graphql-codegen/plugin-helpers'
+import type { PluginFunction } from '@graphql-codegen/plugin-helpers'
 import type { DocumentNode } from 'graphql'
 import { generateNormalizedOperations } from './generateNormalizedOperation'
 import { generateNormalizedMetadataForDocs } from './generateNormalizedMetadata'
 import type { NormalizeMetaShape } from '../metadataShapes'
 
-export const NormalizeCodegenPlugin: CodegenPlugin['plugin'] = (schema, documents, config, info) => {
+const VALID_FORMATS = ['base64', 'stringify', 'json'] as const
+
+export const NormalizeCodegenPlugin: PluginFunction<{
+  format?: (typeof VALID_FORMATS)[number]
+  defaultKeys?: string[]
+  typeKeys?: Record<string, string | string[] | null>
+}> = (schema, documents, config, info) => {
   const documentNodes = documents.map((d) => d.document).filter((d): d is DocumentNode => Boolean(d))
   const typePolicies = {
     defaultKeys: config.defaultKeys,
     typeKeys: config.typeKeys,
   }
+  if (config.format && !VALID_FORMATS.includes(config.format)) {
+    throw new Error(`Invalid format option provided, expected one of ${VALID_FORMATS}`)
+  }
   const ops = generateNormalizedOperations(schema, documentNodes, typePolicies)
   const normalizedMeta = generateNormalizedMetadataForDocs(schema, ops, typePolicies)
-  const toPrint: Record<string, NormalizeMetaShape> = {}
+  const toPrint: Record<string, NormalizeMetaShape | string> = {}
   for (const { name, meta } of normalizedMeta) {
-    toPrint[name] = meta
+    if (config.format && config.format !== 'json') {
+      const jsonString = JSON.stringify(meta)
+      toPrint[name] = config.format === 'base64' ? Buffer.from(jsonString, 'utf-8').toString('base64') : jsonString
+    } else {
+      toPrint[name] = meta
+    }
   }
   return JSON.stringify(toPrint, null, 2)
 }
--- tests/codegen.spec.ts
+++ tests/codegen.spec.ts
@@ -28,4 +28,48 @@ describe('codegen', async () => {
     // fs.writeFileSync(filename, res)
     expect(res).toEqual(fs.readFileSync(filename, 'utf-8'))
   })
+
+  it('should accept an option to stringify the meta', async () => {
+    const filename = path.join(__dirname, 'codegen/out-stringify.json')
+    const res = await codegen({
+      schema: parse(printSchema(schema)),
+      documents: [{ document: operation1Doc }, { document: operation2Doc }, { document: operationWithFrag }],
+      filename,
+      plugins: [
+        {
+          'graphql-normalize': {
+            format: 'stringify',
+          },
+        },
+      ],
+      config: {},
+      pluginMap: {
+        'graphql-normalize': codegenPlugin,
+      },
+    })
+    // fs.writeFileSync(filename, res)
+    expect(res).toEqual(fs.readFileSync(filename, 'utf-8'))
+  })
+
+  it('should accept an option to base64 the meta', async () => {
+    const filename = path.join(__dirname, 'codegen/out-base64.json')
+    const res = await codegen({
+      schema: parse(printSchema(schema)),
+      documents: [{ document: operation1Doc }, { document: operation2Doc }, { document: operationWithFrag }],
+      filename,
+      plugins: [
+        {
+          'graphql-normalize': {
+            format: 'base64',
+          },
+        },
+      ],
+      config: {},
+      pluginMap: {
+        'graphql-normalize': codegenPlugin,
+      },
+    })
+    // fs.writeFileSync(filename, res)
+    expect(res).toEqual(fs.readFileSync(filename, 'utf-8'))
+  })
 })
